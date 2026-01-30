50fefb905a1deabc964e1d352965c47105837ac2
Andrii Loborchuk <loborchuka@gmail.com>
2026-01-30 19:20:27 +0200

    Added opening window and right config for cmake

--- main.cpp
+++ main.cpp
@@ -1,9 +1,32 @@
+#include <SDL3/SDL.h>
 #include <iostream>
 
-using namespace std;
+int main(int argc, char* argv[]) {
+    if (SDL_Init(SDL_INIT_VIDEO) == false) { // SDL3 returns bool
+        std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
+        return 1;
+    }
 
-int main()
-{
-    cout << "Hello World!" << endl;
+    // SDL3 CreateWindow only takes title, width, height, and flags
+    SDL_Window* window = SDL_CreateWindow("CMake SDL window", 800, 600, 0);
+
+    if (!window) {
+        std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+
+    bool quit = false;
+    SDL_Event e;
+    while (!quit) {
+        while (SDL_PollEvent(&e)) {
+            if (e.type == SDL_EVENT_QUIT) { // Renamed in SDL3
+                quit = true;
+            }
+        }
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
 }
